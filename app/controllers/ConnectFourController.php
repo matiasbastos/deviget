2debcce3d57f10f370c48d535bc09b901941ff41
Matias Bastos <matias.bastos@gmail.com>
2015-08-16 20:56:36 -0400

    added winner check horizontal and vertical but diagonal not working yet...

--- app/controllers/ConnectFourController.php
+++ app/controllers/ConnectFourController.php
@@ -5,14 +5,16 @@ class ConnectFourController extends BaseController {
     public function new_game() {
         Session::flush();
         $board = [
-            'l1'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r7'=>0,'r8'=>0],
-            'l2'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r7'=>0,'r8'=>0],
-            'l3'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r7'=>0,'r8'=>0],
-            'l4'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r7'=>0,'r8'=>0],
-            'l5'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r7'=>0,'r8'=>0],
-            'l6'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r7'=>0,'r8'=>0],
+            'l1'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r6'=>0,'r7'=>0],
+            'l2'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r6'=>0,'r7'=>0],
+            'l3'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r6'=>0,'r7'=>0],
+            'l4'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r6'=>0,'r7'=>0],
+            'l5'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r6'=>0,'r7'=>0],
+            'l6'=>['r1'=>0,'r2'=>0,'r3'=>0,'r4'=>0,'r5'=>0,'r6'=>0,'r7'=>0],
         ];
         Session::put('board', $board);
+        Session::put('turn', 1);
+        Session::put('winner', 0);
         $this->layout->content = View::make('home', ['board'=>Session::get('board')]);
     }
 
@@ -20,17 +22,37 @@ class ConnectFourController extends BaseController {
         $this->layout->content = View::make('board', [
             'board'=>Session::get('board'), 
             'player'=>$player, 
-            'turn'=>'Your Turn!'
+            'turn'=>Session::get('turn'),
+            'winner'=>Session::get('winner')
         ]);
     }     
 
     public function add_disc($player, $row) {
+        Profiler::disable();
         try{
+            // check if the game is over
+            if(Session::get('winner')>0) return "There is a winner!";
+            // check turn
+            if(Session::get('turn')!=$player) return "Not Your Turn!";
             $board = Session::get('board');
+            // add teh disc from the bottom to the top
             for($l=6; $l>0; $l--){
                 if($board["l{$l}"]["r{$row}"]==0){
+                    // add disc
                     $board["l{$l}"]["r{$row}"] = $player;
+                    // update session
                     Session::put('board', $board);
+                    // winner?
+                    if($this->check_winner($player)) {
+                        Session::put('winner', $player);
+                        return "Player {$player} Wins!";
+                    }
+                    // change turn
+                    if(Session::get('turn')==1){
+                        Session::put('turn', 2);
+                    }else{
+                        Session::put('turn', 1);
+                    }
                     break;
                 }
             }
@@ -39,5 +61,100 @@ class ConnectFourController extends BaseController {
         }
         return "ok";
     }    
+    
+    private function check_winner($player) {
+        // check horizontal four
+        $board = Session::get('board');
+        return ($this->check_horizontal($player, $board) || $this->check_vertical($player, $board));
+    }
+
+    private function check_horizontal($player, $board) {
+        // starting from line 6
+        for($l=6; $l>0; $l--){
+            $in_line = 0;
+            // for each row
+            foreach($board["l{$l}"] as $rk=>$rv){
+                if($rv==$player){
+                    // add one
+                    $in_line++;
+                }else{
+                    // reset counter
+                    $in_line = 0;
+                }
+                //winner?
+                if($in_line==4) return true;
+            }    
+        }
+    }
+
+    private function check_vertical($player, $board) {
+        // starting from row 1
+        for($r=1; $r<8; $r++){
+            $in_line = 0;
+            // for each line
+            for($l=6; $l>0; $l--){
+                if($board["l{$l}"]["r{$r}"]==$player){
+                    // add one
+                    $in_line++;
+                }else{
+                    // reset counter
+                    $in_line = 0;
+                }
+                //winner?
+                if($in_line==4) return true;
+            }    
+        }
+    }
+
+    private function check_diagonal($player, $board) {
+        // for each row
+        for($r=1; $r<8; $r++){
+            // for each line
+            for($l=6; $l>0; $l--){
+                // there is a disc?
+                if($board["l{$l}"]["r{$r}"]==$player) {
+                    // winner?
+                    if($this->check_diagonal_from($player, $board, $r, $l)) {
+                        return true;
+                    }
+                }
+            }    
+        }
+    }
+
+    private function check_diagonal_from($player, $board, $row, $line) {
+        // to the right
+        $l = $line;
+        for($r=$row; $r<8; $r++){
+            $in_line = 0;
+            if($board["l{$l}"]["r{$r}"]==$player){
+                // add one
+                $in_line++;
+                $l--;
+            }else{
+                // reset counter
+                $in_line = 0;
+                $l = $line;
+            }
+            //winner?
+            if($in_line==4) return true;
+        }
+        // to the left
+        $l = $line;
+        for($r=7; $r>0; $r--){
+            $in_line = 0;
+            if($board["l{$l}"]["r{$r}"]==$player){
+                // add one
+                $in_line++;
+                $l--;
+            }else{
+                // reset counter
+                $in_line = 0;
+                $l = $line;
+            }
+            //winner?
+            if($in_line==4) return true;
+        }
+    }
 }
 
--- app/views/board.blade.php
+++ app/views/board.blade.php
@@ -1,16 +1,53 @@
 @extends('layouts.master')
 @section('content')
     <h1>Connect Four - Player {{$player}}</h1>
-    <h2>{{$turn}}</h2>
+    @if($winner==0)
+        <h2>@if($turn==$player) Your Turn! @else Player {{$turn}} Turn. @endif</h2>
+    @else
+        <h2>Player {{$winner}} Wins! <a href="/">Start A New Game.</a></h2>
+    @endif
+    <input type="hidden" id="player" value="{{$player}}"/>
+    <span class="arrow-down" data-row="1"></span>
+    <span class="arrow-down" data-row="2"></span>
+    <span class="arrow-down" data-row="3"></span>
+    <span class="arrow-down" data-row="4"></span>
+    <span class="arrow-down" data-row="5"></span>
+    <span class="arrow-down" data-row="6"></span>
+    <span class="arrow-down" data-row="7"></span>
     @foreach($board as $lk=>$lv)
         <div class="board-line">
             @foreach($lv as $rk=>$rv)
-                <span class="board-row row-val{{$rv}}" data-line="{{$lk}}" data-row="{{$rk}}"></span>
+                <span class="board-row row-val{{$rv}}" data-line="{{substr($lk, 1)}}" data-row="{{substr($rk, 1)}}"></span>
             @endforeach
         </div>    
     @endforeach
     <a href="/">New Game</a>
+    <script>
+        $(document).ready(function(){
+            $('.arrow-down').click(function() {
+                row = parseInt($(this).data("row"));
+                player = $("#player").val();
+                $.get("/add_disc/"+player+"/"+row, function(data, status){
+                    if(data!="ok") alert("Data: " + data + "\nStatus: " + status);
+                    location.reload();
+                });
+            });
+            @if($winner==0)
+                setInterval(function(){ location.reload(); }, 5000); // I should go to jail for this line
+            @endif
+        });
+    </script>
     <style>
+        .arrow-down {
+            width: 0; 
+            height: 0; 
+            border-left: 20px solid transparent;
+            border-right: 20px solid transparent;
+            border-top: 20px solid #0088cc;
+            margin: 5px;
+            cursor: pointer;
+            display: inline-block;
+        }
         .board-row {
             width: 50px;
             height: 50px;
@@ -27,5 +64,4 @@
             background: yellow;
         }
     </style>
-    <xmp>{{print_r($board)}}</xmp> 
 @stop
